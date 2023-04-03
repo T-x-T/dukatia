14b69a4972f3e132d079c789125994591d092167
T-x-T <37040316+T-x-T@users.noreply.github.com>
2023-04-03 20:13:07 +0200

    Implements #12

--- frontend/pages/accounts/_id.vue
+++ frontend/pages/accounts/_id.vue
@@ -29,3 +29,9 @@ export default {
 	}
 }
 </script>
+
+<style lang="sass" scoped>
+div#main
+	height: 100vh
+	overflow: scroll
+</style>
--- frontend/pages/accounts/index.vue
+++ frontend/pages/accounts/index.vue
@@ -32,13 +32,15 @@ export default {
 					{name: "ID", type: "number"},
 					{name: "Name", type: "string"},
 					{name: "Currency", type: "choice", options: this.$store.state.currencies.map(x => x.name)},
-					{name: "Tags", type: "choice", options: [...new Set(this.$store.state.tags.map(x => x.name))]}
+					{name: "Tags", type: "choice", options: [...new Set(this.$store.state.tags.map(x => x.name))]},
+					{name: "Balance", type: "number"}
 				],
 				rows: this.$store.state.accounts.map(x => ([
 					x.id,
 					x.name,
 					this.$store.state.currencies.filter(c => c.id == x.default_currency_id)[0].name,
-					this.$store.state.tags.filter(y => x.tag_ids?.includes(y.id)).map(y => y.name).join(", ")
+					this.$store.state.tags.filter(y => x.tag_ids?.includes(y.id)).map(y => y.name).join(", "),
+					this.$store.state.transactions.filter(t => t.account_id == x.id).reduce((a, b) => a + b.amount, 0) / this.$store.state.currencies.filter(c => c.id == x.default_currency_id)[0].minor_in_mayor + this.$store.state.currencies.filter(c => c.id == x.default_currency_id)[0].symbol
 				]))
 			}
 		},
