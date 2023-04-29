57a339f6c2ee041fb5786a97eaafc9a9c418154f
T-x-T <37040316+T-x-T@users.noreply.github.com>
2023-04-29 20:44:10 +0200

    Mark currently active route in again in nav

--- frontend/app.vue
+++ frontend/app.vue
@@ -4,15 +4,15 @@
 			<div id="navcontainer">
 				<h1>TxTs Treasury</h1>
 				<ul v-if="loggedIn">
-					<li ref="dashboard" @click="currentRoute = 'dashboard'"><NuxtLink to="/">Dashboard</NuxtLink></li>
-					<li ref="transactions" @click="currentRoute = 'transactions'"><NuxtLink to="/transactions">Transactions</NuxtLink></li>
-					<li ref="assets" @click="currentRoute = 'assets'"><NuxtLink to="/assets">Assets</NuxtLink></li>
-					<li ref="accounts" @click="currentRoute = 'accounts'"><NuxtLink to="/accounts">Accounts</NuxtLink></li>
-					<li ref="recipients" @click="currentRoute = 'recipients'"><NuxtLink to="/recipients">Recipients</NuxtLink></li>
-					<li ref="tags" @click="currentRoute = 'tags'"><NuxtLink to="/tags">Tags</NuxtLink></li>
-					<li ref="currencies" @click="currentRoute = 'currencies'"><NuxtLink to="/currencies">Currencies</NuxtLink></li>
+					<li ref="dashboard" @click="currentRoute = 'dashboard'"><NuxtLink to="/" :class="currentRoute == 'dashboard' ? 'active' : ''">Dashboard</NuxtLink></li>
+					<li ref="transactions" @click="currentRoute = 'transactions'"><NuxtLink to="/transactions" :class="currentRoute == 'transactions' ? 'active' : ''">Transactions</NuxtLink></li>
+					<li ref="assets" @click="currentRoute = 'assets'"><NuxtLink to="/assets" :class="currentRoute == 'assets' ? 'active' : ''">Assets</NuxtLink></li>
+					<li ref="accounts" @click="currentRoute = 'accounts'"><NuxtLink to="/accounts" :class="currentRoute == 'accounts' ? 'active' : ''">Accounts</NuxtLink></li>
+					<li ref="recipients" @click="currentRoute = 'recipients'"><NuxtLink to="/recipients" :class="currentRoute == 'recipients' ? 'active' : ''">Recipients</NuxtLink></li>
+					<li ref="tags" @click="currentRoute = 'tags'"><NuxtLink to="/tags" :class="currentRoute == 'tags' ? 'active' : ''">Tags</NuxtLink></li>
+					<li ref="currencies" @click="currentRoute = 'currencies'"><NuxtLink to="/currencies" :class="currentRoute == 'currencies' ? 'active' : ''">Currencies</NuxtLink></li>
 					<br><br>
-					<li ref="settings" @click="currentRoute = 'settings'"><NuxtLink to="/settings">Settings</NuxtLink></li>
+					<li ref="settings" @click="currentRoute = 'settings'"><NuxtLink to="/settings" :class="currentRoute == 'settings' ? 'active' : ''">Settings</NuxtLink></li>
 					<li @click="logout"><a>Logout</a></li>
 				</ul>
 			</div>
@@ -26,15 +26,21 @@
 </template>
 
 <script lang="ts" setup>
-
-let currentRoute = "dashboard";
-let loggedIn = document.cookie.includes("accessToken");
+const loggedIn = document.cookie.includes("accessToken");
 
 if(!loggedIn) useRouter().replace("/login");
 
 function logout() {
 	document.cookie = "accessToken=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
 	location.reload();
+}
+</script>
+
+<script lang="ts">
+export default {
+	data: () => ({
+		currentRoute: useRoute().path.split("/")[1] ? useRoute().path.split("/")[1] : "dashboard",
+	})
 }
 </script>
 
