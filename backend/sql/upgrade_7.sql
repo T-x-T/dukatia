0a48580a4f26856daf9868b46ee572cedfe6eeb0
T-x-T <37040316+T-x-T@users.noreply.github.com>
2023-05-27 23:22:44 +0200

    remove upgrade_7.sql because its actually not needed

--- backend/sql/upgrade_7.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE public.dashboards DISABLE TRIGGER ALL;
-INSERT INTO public.dashboards VALUES (DEFAULT, 0, 'Default', 'The default Dashboard');
-ALTER TABLE public.dashboards ENABLE TRIGGER ALL;
