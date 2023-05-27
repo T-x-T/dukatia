5a3be7bbab577d3a1cb2c15896ea59293da32a89
T-x-T <37040316+T-x-T@users.noreply.github.com>
2023-05-27 23:12:19 +0200

    fix upgrade_7.sql

--- backend/sql/upgrade_7.sql
+++ backend/sql/upgrade_7.sql
@@ -1,1 +1,3 @@
+ALTER TABLE public.dashboards DISABLE TRIGGER ALL;
 INSERT INTO public.dashboards VALUES (DEFAULT, 0, 'Default', 'The default Dashboard');
+ALTER TABLE public.dashboards ENABLE TRIGGER ALL;
