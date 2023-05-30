4469a91058f6169f5e7ae8f42ed84aa7f69fa6a8
T-x-T <37040316+T-x-T@users.noreply.github.com>
2023-05-30 20:46:08 +0200

    Implements #39

---
+++ frontend/public/worker.js
@@ -0,0 +1,43 @@
+const putInCache = async (request, response) => {
+  const cache = await caches.open("v1");
+  await cache.put(request, response);
+};
+
+const cacheFirst = async ({ request, fallbackUrl }) => {
+  // First try to get the resource from the cache.
+
+  // If the response was not found in the cache,
+  // try to get the resource from the network.
+  try {
+    const responseFromNetwork = await fetch(request);
+    // If the network request succeeded, clone the response:
+    // - put one copy in the cache, for the next time
+    // - return the original to the app
+    // Cloning is needed because a response can only be consumed once.
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  } catch (error) {
+    // If the network request failed,
+    // get the fallback response from the cache.
+    const fallbackResponse = await caches.match(fallbackUrl);
+    if (fallbackResponse) {
+      return fallbackResponse;
+    }
+    // When even the fallback response is not available,
+    // there is nothing we can do, but we must always
+    // return a Response object.
+    return new Response("Network error happened", {
+      status: 408,
+      headers: { "Content-Type": "text/plain" },
+    });
+  }
+};
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    cacheFirst({
+      request: event.request,
+      fallbackUrl: "/fallback.html",
+    })
+  );
+});
