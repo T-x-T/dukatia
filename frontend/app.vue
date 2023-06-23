4c8e6e9baa30c22b889ee4dd47e59b4bb5049527
T-x-T <37040316+T-x-T@users.noreply.github.com>
2023-06-23 22:43:26 +0200

    Make NavBar collapsable and fullscreen on small screens

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,22 +1,8 @@
 <template>
 	<main>
-		<nav>
-			<div id="navcontainer">
-				<h1>TxTs Treasury</h1>
-				<ul v-if="loggedIn">
-					<li ref="dashboard" @click="currentRoute = 'dashboard'"><NuxtLink to="/" :class="currentRoute == 'dashboard' ? 'active' : ''">Dashboard</NuxtLink></li>
-					<li ref="transactions" @click="currentRoute = 'transactions'"><NuxtLink to="/transactions" :class="currentRoute == 'transactions' ? 'active' : ''">Transactions</NuxtLink></li>
-					<li ref="assets" @click="currentRoute = 'assets'"><NuxtLink to="/assets" :class="currentRoute == 'assets' ? 'active' : ''">Assets</NuxtLink></li>
-					<li ref="accounts" @click="currentRoute = 'accounts'"><NuxtLink to="/accounts" :class="currentRoute == 'accounts' ? 'active' : ''">Accounts</NuxtLink></li>
-					<li ref="recipients" @click="currentRoute = 'recipients'"><NuxtLink to="/recipients" :class="currentRoute == 'recipients' ? 'active' : ''">Recipients</NuxtLink></li>
-					<li ref="tags" @click="currentRoute = 'tags'"><NuxtLink to="/tags" :class="currentRoute == 'tags' ? 'active' : ''">Tags</NuxtLink></li>
-					<li ref="currencies" @click="currentRoute = 'currencies'"><NuxtLink to="/currencies" :class="currentRoute == 'currencies' ? 'active' : ''">Currencies</NuxtLink></li>
-					<br><br>
-					<li ref="settings" @click="currentRoute = 'settings'"><NuxtLink to="/settings" :class="currentRoute == 'settings' ? 'active' : ''">Settings</NuxtLink></li>
-					<li @click="logout"><a>Logout</a></li>
-				</ul>
-			</div>
-		</nav>
+		<SideNav 
+			:loggedIn="loggedIn"		
+		/>
 		<div id="content">
 			<div id="innerContent">
 				<NuxtPage />
@@ -29,12 +15,6 @@
 const loggedIn = document.cookie.includes("accessToken");
 
 if(!loggedIn) useRouter().replace("/login");
-
-function logout() {
-	document.cookie = "accessToken=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-	location.reload();
-}
-
 
 const registerServiceWorker = async () => {
   if ("serviceWorker" in navigator) {
@@ -56,54 +36,25 @@ const registerServiceWorker = async () => {
 };
 
 registerServiceWorker();
-
-</script>
-
-<script lang="ts">
-export default {
-	data: () => ({
-		currentRoute: useRoute().path.split("/")[1] ? useRoute().path.split("/")[1] : "dashboard",
-	})
-}
 </script>
 
 <style lang="sass" scoped>
-@import "assets/_vars.sass"
-
 main
 	display: flex
 	height: 100vh
 	width: 100vw
 	overflow: hidden
-
-nav
-	width: 21em
-	min-height: 100vh
-	h1
-		margin: 25px
-		cursor: pointer
-		user-select: none
-		text-shadow: 4px 4px 8px black
-		&:hover
-			transform: scale(1.1) rotate(5deg)
-	a
-		@extend .semibold
-		font-size: 24px
-		margin: 10px
-		margin-left: 20px
-		transition-duration: 0.2s
-		cursor: pointer
-
-li
-	width: fit-content
-	&:hover
-		transform: scale(1.1)
+	@media screen and (max-width: 800px)
+		flex-direction: column
 
 #content
 	flex-grow: 1
 	width: 1rem
 	height: 100vh
 	overflow: auto
+	@media screen and (max-width: 800px)
+		height: 1rem
+		width: 100vw
 
 #innerContent
 	margin: 10px 10px 10px 10px
