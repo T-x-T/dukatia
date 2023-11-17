eba4183224d3185450bc5a2078f1c6f6288479d2
T-x-T <37040316+T-x-T@users.noreply.github.com>
2023-11-17 15:34:13 +0100

    Refactor Buy/Sell and Update Valuation Form out of AssetDetails

--- frontend/components/AssetDetails.vue
+++ frontend/components/AssetDetails.vue
@@ -1,72 +1,41 @@
 <template>
-	<div>
+	<div id="wrapper">
 		<button @click="$emit('back')">Back</button>
 		<button @click="showAssetValuationEditor = true" class="mobile_hidden">Edit Asset Valuations</button>
 		<div id="grid">
 			<div class="gridItem form">
-				<div id="inner">
-					<h3>Asset data</h3>
-					<AssetForm
-						v-if="Object.keys(asset).length > 0"
-						:data="asset"
-						@back="$emit('back')"
-						@data_saved="reload"
-					/>
-				</div>
+				<h3>Asset data</h3>
+				<AssetForm
+					v-if="Object.keys(asset).length > 0"
+					:data="asset"
+					@back="$emit('back')"
+					@data_saved="reload"
+				/>
 			</div>
 				
 			<div v-if="asset?.id !== undefined && renderCharts" class="gridItem form">
-				<div id="inner">
-					<h3>Buy/Sell with transaction</h3>
-					<div id="transactionForm">
-						<label for="transactionAmount">Amount change:</label>
-						<input type="number" id="transactionAmount" v-model="transactionData.amount" @input="updateTransactionTotal">
-						<br>
-						<label>Value per unit:</label>
-						<InputMoney :initial_value="transactionData.value_per_unit" v-on:update="((new_value: Money) => {transactionData.value_per_unit = new_value; updateTransactionTotal()})" />
-						<br>
-						<label>Additional cost:</label>
-						<InputMoney :initial_value="transactionData.cost" v-on:update="((new_value: Money) => {transactionData.cost = new_value; updateTransactionTotal()})" />
-						<br>
-						<label for="transactionAccount">Account:</label>
-						<select id="transactionAccount" v-model="transactionData.account_id">
-							<option v-for="(account, index) in accounts.filter(x => x.default_currency_id === asset.currency_id)" :key="index" :value="account.id">{{account.name}}</option>
-						</select>
-						<br>
-						<label for="transactionTimestamp">Timestamp:</label>
-						<input type="datetime-local" id="transactionTimestamp" v-model="transactionData.timestamp">
-						<br>
-						<label>Total:</label>
-						<InputMoney :initial_value="transactionData.total" v-on:update="((new_value: Money) => {transactionData.total_manually_changed = true; transactionData.total = new_value; updateTransactionTotal()})" />
-						<br>
-						<button class="green" @click="saveTransaction">Save</button>
-					</div>
-				</div>
+				<AssetBuySellForm 
+					:asset="asset"
+					@saved="reload"
+				/>
 			</div>
 			<div v-if="asset?.id !== undefined && renderCharts" class="gridItem form">
-				<div id="inner">
-					<h3>Update without transaction</h3>
-					<div id="updateForm">
-						<label for="updateAmount">New amount:</label>
-						<input type="number" id="updateAmount" v-model="updateData.amount">
-						<br>
-						<label>Value per unit:</label>
-						<InputMoney :initial_value="updateData.value_per_unit" v-on:update="((new_value: Money) => updateData.value_per_unit = new_value)" />
-						<br>
-						<label for="updateTimestamp">Timestamp:</label>
-						<input type="datetime-local" id="updateTimestamp" v-model="updateData.timestamp">
-						<br>
-						<button class="green" @click="saveUpdate">Save</button>
-					</div>
-				</div>
+				<AssetUpdateValuationForm
+					:asset="asset"
+					@saved="reload"
+				/>
 			</div>
 			<div v-if="asset?.id !== undefined && renderCharts" class="gridItem chart">
 				<Chart
 					:chart_options="asset_total_value_chart"
 				/>
+			</div>
+			<div v-if="asset?.id !== undefined && renderCharts" class="gridItem chart">
 				<Chart
 					:chart_options="asset_single_value_chart"
 				/>
+			</div>
+			<div v-if="asset?.id !== undefined && renderCharts" class="gridItem chart">
 				<Chart
 					:chart_options="asset_amount_chart"
 				/>
@@ -87,11 +56,8 @@
 export default {
 	data: () => ({
 		asset: {} as Asset,
-		transactionData: {} as {[key: string]: any},
-		updateData: {} as {[key: string]: any},
 		renderCharts: false,
 		showAssetValuationEditor: false,
-		accounts: [] as Account[],
 		asset_total_value_chart: {} as ChartOptions,
 		asset_single_value_chart: {} as ChartOptions,
 		asset_amount_chart: {} as ChartOptions,
@@ -109,14 +75,7 @@ export default {
 	},
 
 	methods: {
-		async update() {
-			try {
-				this.accounts = await $fetch("/api/v1/accounts/all");
-			} catch(e: any) {
-				console.error(e?.data?.data);
-				window.alert(e?.data?.data?.error);
-			}
-			
+		async update() {			
 			this.asset = Object.keys(this.asset).length > 0 ? this.asset : this.propAsset;
 
 			if(!this.asset) {
@@ -125,21 +84,6 @@ export default {
 			} else {
 				if(this.asset.value_per_unit === undefined) this.asset.value_per_unit = {major: 0, minor: 0, minor_in_major: 100, symbol: "€"};
 			}
-
-			this.transactionData = {
-				amount: 0,
-				value_per_unit: this.asset.value_per_unit,
-				timestamp: new Date(Date.now() - new Date().getTimezoneOffset() * 60000).toISOString().slice(0, -8),
-				account_id: 0,
-				cost: {major: 0, minor: 0, minor_in_major: this.asset.value_per_unit.minor_in_major, symbol: this.asset.value_per_unit.symbol},
-				total: {major: 0, minor: 0, minor_in_major: this.asset.value_per_unit.minor_in_major, symbol: this.asset.value_per_unit.symbol},
-			};
-
-			this.updateData = {
-				amount: this.asset.amount,
-				value_per_unit: this.asset.value_per_unit,
-				timestamp: new Date(Date.now() - new Date().getTimezoneOffset() * 60000).toISOString().slice(0, -8)
-			};
 
 			this.asset_total_value_chart = await $fetch("/api/v1/charts/7");
 			this.asset_total_value_chart.asset_id = this.asset.id;
@@ -160,62 +104,6 @@ export default {
 			this.$nextTick(() => this.renderCharts = true);
 		},
 
-		async saveTransaction() {
-			try {
-				await $fetch(`/api/v1/assets/${this.asset.id}/valuations`, {
-					method: "POST",
-					body: {
-						amount_change: Number(this.transactionData.amount),
-						value_per_unit: this.transactionData.value_per_unit,
-						timestamp: new Date(this.transactionData.timestamp),
-						account_id: this.transactionData.account_id,
-						cost: this.transactionData.cost,
-						total_value: this.transactionData.total_manually_changed ? this.transactionData.total : null
-					}
-				})
-			} catch(e: any) {
-				console.error(e?.data?.data);
-				window.alert(e?.data?.data?.error);
-				return;
-			}
-
-			this.reload();
-		},
-
-		updateTransactionTotal() {
-			this.transactionData.total_manually_changed = false;
-			const raw_value_per_unit: number = (this.transactionData.value_per_unit.major * this.transactionData.value_per_unit.minor_in_major) + this.transactionData.value_per_unit.minor;
-			const raw_cost: number = (this.transactionData.cost.major * this.transactionData.cost.minor_in_major) + this.transactionData.cost.minor;
-			const raw_total = Math.round(((Number(this.transactionData.amount) * raw_value_per_unit) + raw_cost) * -100 + Number.EPSILON) / 100;
-			
-			this.transactionData.total = {
-				major: (raw_total / this.transactionData.value_per_unit.minor_in_major).toFixed(0),
-				minor: raw_total < 0 ? (raw_total % this.transactionData.value_per_unit.minor_in_major) * -1 : (raw_total % this.transactionData.value_per_unit.minor_in_major),
-				minor_in_major: this.transactionData.value_per_unit.minor_in_major,
-				symbol: this.transactionData.value_per_unit.symbol,
-				is_negative: raw_total < 0,
-			};
-		},
-
-		async saveUpdate() {
-			try {
-				await $fetch(`/api/v1/assets/${this.asset.id}/valuations`, {
-					method: "POST",
-					body: {
-						amount: Number(this.updateData.amount),
-						value_per_unit: this.updateData.value_per_unit,
-						timestamp: new Date(this.updateData.timestamp)
-					}
-				})
-			} catch(e: any) {
-				console.error(e?.data?.data);
-				window.alert(e?.data?.data?.error);
-				return;
-			}
-
-			this.reload();
-		},
-
 		closeAssetValuationEditor() {
 			this.showAssetValuationEditor = false;
 			this.reload();
@@ -225,6 +113,8 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+div#wrapper
+	margin: 10px
 
 div#grid
 	display: flex
@@ -235,19 +125,11 @@ div#grid
 	gap: 10px
 	flex-wrap: wrap
 
-div.form
-	display: flex
-	align-items: center
-	justify-content: center
-	height: max-content
-
 div.gridItem
 	padding: 10px
 
 div.chart
 	flex-grow: 1
-
-div.chart
 	width: 50vw
 	height: 40vh
 </style>
---
+++ frontend/components/AssetUpdateValuationForm.vue
@@ -0,0 +1,81 @@
+<template>
+	<div id="wrapper">
+		<h3>Update without transaction</h3>
+		<div>
+			<label>
+				New amount:
+				<input type="number" v-model="update_data.amount">
+			</label>
+			
+			<label>
+				Value per unit:
+				<InputMoney 
+					:initial_value="update_data.value_per_unit"
+					v-on:update="((new_value: Money) => update_data.value_per_unit = new_value)"
+					/>
+			</label>
+			
+			<label>
+				Timestamp:
+				<input type="datetime-local" v-model="update_data.timestamp">
+			</label>
+			
+			<button class="green" @click="save">Save</button>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+export default {
+	data: () => ({
+		update_data: {} as {[key: string]: any},
+	}),
+
+	$emits: ["saved"],
+
+	props: {
+		asset: {
+			type: Object as PropType<Asset>,
+			required: true,
+		}
+	},
+
+	created() {
+		this.update_data = {
+			amount: this.asset.amount,
+			value_per_unit: this.asset.value_per_unit,
+			timestamp: new Date(Date.now() - new Date().getTimezoneOffset() * 60000).toISOString().slice(0, -8)
+		};
+	},
+
+	methods: {
+		async save() {
+			try {
+				await $fetch(`/api/v1/assets/${this.asset.id}/valuations`, {
+					method: "POST",
+					body: {
+						amount: Number(this.update_data.amount),
+						value_per_unit: this.update_data.value_per_unit,
+						timestamp: new Date(this.update_data.timestamp)
+					}
+				});
+				this.$emit("saved");
+			} catch(e: any) {
+				console.error(e);
+				window.alert(e?.data);
+				return;
+			}
+		},
+	},
+}
+</script>
+
+<style lang="sass" scoped>
+div#wrapper
+	width: 350px
+
+label
+	display: flex
+	input, select
+		flex-grow: 1
+</style>
