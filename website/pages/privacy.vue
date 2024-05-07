47a678a3f01ba561b1c05b43ad1cc92d9848292a
T-x-T <37040316+T-x-T@users.noreply.github.com>
2024-05-07 20:13:28 +0200

    a basic privacy policy and legal notice

--- website/pages/privacy.vue
+++ website/pages/privacy.vue
@@ -1,3 +1,31 @@
 <template>
 	<h1>Privacy Policy</h1>
+	<div id="policy">
+		<p>Last updated: May 07, 2024</p>
+		<br>
+		<p>I do not collect any information about you myself. You are free to audit the source code on <a href="https://github.com/T-x-T/dukatia" target="_blank" rel="noopener noreferrer">Github</a> to verify this fact yourself.</p>
+		<p>This website however is hosted on cloudflare pages, so their <a href="https://www.cloudflare.com/privacypolicy/" target="_blank" rel="noopener noreferrer">privacy policy</a> also applies.</p>
+		<p>The source code and docker images are hosted on Github, so their <a href="https://docs.github.com/en/site-policy/privacy-policies/github-general-privacy-statement" target="_blank" rel="noopener noreferrer">privacy policy</a> applies too.</p>
+		<p>If you have any questions, concerns or other inquiries please contact me via E-Mail at <a href="mailto:contact@thetxt.io">contact@thetxt.io</a>.</p>
+	</div>
 </template>
+
+<style lang="sass" scoped>
+h1
+	margin: 125px 0 25px 0
+	font-size: 5em
+	text-align: center
+
+div#policy
+	margin: 25px
+
+p, a
+	text-align: center
+
+a
+	margin: 0
+	text-decoration: underline
+
+div#policy
+	margin-bottom: 50vh
+</style>
