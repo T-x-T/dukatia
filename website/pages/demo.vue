2f92fbce437766012a37cbf53a8151adb3b4a849
T-x-T <37040316+T-x-T@users.noreply.github.com>
2024-05-07 19:32:22 +0200

    add demo placeholder  site

--- website/pages/demo.vue
+++ website/pages/demo.vue
@@ -1,3 +1,28 @@
 <template>
-	<h1>Demo</h1>
+	<div id="container">
+		<h1>Demo</h1>
+		<p>The demo is coming soon!</p>
+		<p>Until then you could also just try dukatia by running it yourself, for free.</p>
+		<button class="important" @click="navigateTo('/docs')">Get started</button>
+	</div>
 </template>
+
+<style lang="sass" scoped>
+div#container
+	display: flex
+	flex-direction: column
+	align-items: center
+
+h1
+	margin: 25px
+	font-size: 5em
+	text-align: center
+
+p
+	text-align: center
+	font-size: 1.5em
+
+button
+	margin: 25px 0 50vh 0
+	width: fit-content
+</style>
