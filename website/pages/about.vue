6e5cc405723adcd63a69eb3a7cd94c5300ade999
T-x-T <37040316+T-x-T@users.noreply.github.com>
2024-05-08 21:48:04 +0200

    start implementing about page on website

--- website/pages/about.vue
+++ website/pages/about.vue
@@ -1,3 +1,39 @@
 <template>
 	<h1>About</h1>
+		<div id="me">
+			<img id="avatar" src="/avatar.svg" alt="Avatar of TheTxT" />
+			<p id="speechbubble">Hi! On the Internet I call myself TxT and I like to develop software in my spare time. Dukatia is a project that I started in 2021 when I finally grew frustrated of my old way to manage my finances - an Excel sheet.</p>
+		</div>
 </template>
+
+<style lang="sass" scoped>
+@import "assets/_vars.sass"
+
+h1
+	margin: 125px 0 25px 0
+	font-size: 5em
+	text-align: center
+
+div#me
+	display: flex
+
+img#avatar
+	width: 200px
+	filter: invert(100)
+
+p#speechbubble
+	width: 450px
+	font-size: 1.2em
+	padding: 25px
+	margin-left: 50px
+	border-radius: 50px
+	border: 5px solid $dark-darker
+	&::before
+		content:"\A"
+		border-style: solid
+		border-width: 50px 50px 50px 0
+		border-color: transparent $dark-darker transparent transparent
+		position: absolute
+		margin-left: -80px
+		margin-top: 20px
+</style>
