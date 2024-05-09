74c236c13667edf7239186b99ba03a9c60edd133
T-x-T <37040316+T-x-T@users.noreply.github.com>
2024-05-09 12:23:37 +0200

    add content of about page

--- website/pages/about.vue
+++ website/pages/about.vue
@@ -1,9 +1,43 @@
 <template>
 	<h1>About</h1>
-		<div id="me">
+		<section id="me">
 			<img id="avatar" src="/avatar.svg" alt="Avatar of TheTxT" />
 			<p id="speechbubble">Hi! On the Internet I call myself TxT and I like to develop software in my spare time. Dukatia is a project that I started in 2021 when I finally grew frustrated of my old way to manage my finances - an Excel sheet.</p>
-		</div>
+		</section>
+
+		<section id="goals">
+			<div id="goals">
+				<h3>Goals</h3>
+				<p>Dukatia aims to...</p>
+				<p>… be <span>private</span> and <span>transparent</span></p>
+				<p>… be <span>accessible</span> on every device (with a web browser)</p>
+				<p>… provide an <span>easy to digest</span> overview over your financial habits</p>
+				<p>… be <span>resource efficient</span> and <span>performant</span></p>
+			</div>
+			<div id="non_goals">
+				<h3>Non-Goals</h3>
+				<p>Dukatia doesn't try to …</p>
+				<p>… completely automate data ingest</p>
+				<p>… employ AI/ML systems to analyze your data</p>
+				<p>… encrypt all your data (except passwords of course)</p>
+				<p>… require deep financial knowledge to fully utilize it</p>
+			</div>
+		</section>
+
+		<section id="history">
+			<h3>History</h3>
+			<div id="content">
+				<p>
+					In 2021, I started Dukatia under the name “TxT's Treasury”. Since then, a lot has happened both in the backend and the frontend.<br>
+					If you want to dive into the making of Dukatia, I created a video series to document the development process.
+				</p>
+				<picture>
+					<source srcset="/about_yt_playlist_thumbnail.webp" type="image/webp">
+					<source srcset="/about_yt_playlist_thumbnail.jpg" type="image/jpeg"> 
+					<img src="/about_yt_playlist_thumbnail.jpg" alt="Youtube Playlist Thumbnail">
+				</picture>
+			</div>
+		</section>
 </template>
 
 <style lang="sass" scoped>
@@ -14,8 +48,41 @@ h1
 	font-size: 5em
 	text-align: center
 
-div#me
+h3
+	font-size: 4em
+
+p
+	font-size: 1.2em
+
+section
+	margin-top: 50px
+
+section#me
 	display: flex
+	justify-content: center
+
+section#goals
+	display: flex
+	justify-content: space-evenly
+	div#goals > h3
+		color: $success
+	div#non_goals > h3
+		color: $error
+	span
+		color: $success
+
+section#history
+	margin: 50px
+	h3
+		text-align: center
+		margin-bottom: 25px
+	div#content
+		display: grid
+		gap: 50px
+		grid-template-columns: 1fr 1fr
+		img
+			width: 100%
+
 
 img#avatar
 	width: 200px
@@ -23,7 +90,6 @@ img#avatar
 
 p#speechbubble
 	width: 450px
-	font-size: 1.2em
 	padding: 25px
 	margin-left: 50px
 	border-radius: 50px
