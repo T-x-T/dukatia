d2155609a986d270a4cc4b6f4ff9171a963ff8c3
T-x-T <37040316+T-x-T@users.noreply.github.com>
2024-05-10 13:33:27 +0200

    some mobile nav fixes

--- website/components/MobileNav.vue
+++ website/components/MobileNav.vue
@@ -7,13 +7,13 @@
     </div>
     
     <div v-if="nav_open" id="fullscreen_nav">
-      <img v-if="$colorMode.preference == 'dark'" src="/dukatia-beta_logo_white.svg" @click="navigateTo('/')"/>
-      <img v-if="$colorMode.preference == 'light'" src="/dukatia-beta_logo.svg" @click="navigateTo('/')"/>
-      <button class="important" @click="navigateTo('/docs')">Get started</button>
-      <NuxtLink to="/docs">Docs</NuxtLink>
-      <NuxtLink to="/demo">Demo</NuxtLink>
-      <NuxtLink to="/about">About</NuxtLink>
-      <NuxtLink to="/">Home</NuxtLink>
+      <img v-if="$colorMode.preference == 'dark'" src="/dukatia-beta_logo_white.svg" @click="() => {navigateTo('/'); nav_open = false}"/>
+      <img v-if="$colorMode.preference == 'light'" src="/dukatia-beta_logo.svg" @click="() => {navigateTo('/'); nav_open = false}"/>
+      <button class="important" @click="() => {navigateTo('/docs'); nav_open = false}">Get started</button>
+      <NuxtLink @click.native="nav_open = false" to="/docs">Docs</NuxtLink>
+      <NuxtLink @click.native="nav_open = false" to="/demo">Demo</NuxtLink>
+      <NuxtLink @click.native="nav_open = false" to="/about">About</NuxtLink>
+      <NuxtLink @click.native="nav_open = false" to="/">Home</NuxtLink>
       <div id="theme_selector" @click="$colorMode.preference = $colorMode.preference == 'dark' ? 'light' : 'dark'">
         <svg v-if="$colorMode.preference == 'dark'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" /></svg>
         <svg v-if="$colorMode.preference == 'light'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" /></svg>
@@ -68,12 +68,14 @@ export default {
   display: flex
   flex-direction: column
   align-items: center
+  z-index: 1000
   button
     font-size: 3em
     width: max-content
   img
     margin: 25px
     height: 8em
+    max-width: 80vw
   a
     @extend .bold
     font-size: min(5vh, 4em)
