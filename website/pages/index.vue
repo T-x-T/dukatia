40401c6f8fd3079b5e8266a74602192740f16ded
T-x-T <37040316+T-x-T@users.noreply.github.com>
2024-07-09 18:51:15 +0200

    add main image to landing page

--- website/pages/index.vue
+++ website/pages/index.vue
@@ -10,9 +10,12 @@
       </div>
 
       <div id="main_image" class="mobile_hidden">
-        <div id="image_container">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5m.75-9 3-3 2.148 2.148A12.061 12.061 0 0 1 16.5 7.605" /></svg>
-        </div>
+        <ClientOnly>
+          <div id="image_container">
+            <img v-if="$colorMode.preference == 'dark'" src="/dukatia_dashboard_dark.webp"/>
+            <img v-else src="/dukatia_dashboard_light.webp"/>
+          </div>
+        </ClientOnly>
       </div>
     </section>
 
@@ -141,14 +144,17 @@ section#introduction
     div#image_container
       width: 100%
       height: 100%
-      svg
-        height: 80%
+      img
+        width: 100%
+        transform: rotateY(40deg) rotateX(-20deg) scaleX(90%)
+        box-shadow: 0px 0px 200px rgba(0, 0, 0, 0.5)
+        transition-duration: 1s
+        &:hover
+          transform: rotateY(20deg) rotateX(-10deg)
 
 html.light-mode
-  div#main_image, div#introduction
-    background: $light-dark
-    div#container > h2
-      color: white
+  h2
+    color: white
 
 
 
